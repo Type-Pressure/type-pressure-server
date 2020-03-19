f5f0206b4bff98c20485a711b0ce53b5222e9e08
naufalyunan <naufalyunan45@gmail.com>
2020-03-19 14:10:39 +0700

    basic prototype

--- app.js
+++ app.js
@@ -8,12 +8,22 @@ if(env == 'development'){
 	require('dotenv').config()
 }
 
+let position = []
+
+
 io.on('connection', function(socket){
-  console.log('a user connected');
+	console.log('a user connected');
+	socket.emit('position', position)
+	socket.on('move', data => {
+		position.x += data
+		io.emit('position', position)
+	})
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
 });
+
+
 
 http.listen(process.env.PORT, () => console.log(`listening on port ${process.env.PORT}`))
 
