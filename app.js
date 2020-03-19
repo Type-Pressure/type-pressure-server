b198195f38b3520b8c8353e5f35dc1ceefc11c2e
naufalyunan <naufalyunan45@gmail.com>
2020-03-19 10:43:59 +0700

    add app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+const port = 3000
+
+http.listen(port, () => console.log(`listening on port ${port}`))
+
